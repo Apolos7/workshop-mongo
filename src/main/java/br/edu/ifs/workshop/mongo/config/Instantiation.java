fe40f5215b970f38a5f16623092a8fd0be28a039
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-31 19:35:25 -0300

    Perfil de teste com inicialiização da base de dados

---
+++ src/main/java/br/edu/ifs/workshop/mongo/config/Instantiation.java
@@ -0,0 +1,34 @@
+package br.edu.ifs.workshop.mongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.edu.ifs.workshop.mongo.domain.User;
+import br.edu.ifs.workshop.mongo.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Marilia Grey", "mariri_grey@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+	}
+
+}
