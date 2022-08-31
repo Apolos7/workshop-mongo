e20f4477db46b06b229f59e5834e2a08b52118aa
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-31 18:46:12 -0300

    Projeto criado

---
+++ src/test/java/br/edu/ifs/workshop/mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.workshop.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
