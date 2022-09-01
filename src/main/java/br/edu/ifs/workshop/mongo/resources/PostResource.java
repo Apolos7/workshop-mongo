90f2346ea1c9c92e1a33d1869118191d3725f9e5
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-09-01 13:54:46 -0300

    Remoção de endpoints não utilizados

--- src/main/java/br/edu/ifs/workshop/mongo/resources/PostResource.java
+++ src/main/java/br/edu/ifs/workshop/mongo/resources/PostResource.java
@@ -1,19 +1,11 @@
 package br.edu.ifs.workshop.mongo.resources;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifs.workshop.mongo.domain.Post;
 import br.edu.ifs.workshop.mongo.services.PostService;
@@ -25,35 +17,9 @@ public class PostResource {
 	@Autowired
 	private PostService postService;
 
-	@GetMapping
-	public ResponseEntity<List<Post>> findAll() {
-		List<Post> list = postService.findAll();
-		return ResponseEntity.ok(list);
-	}
-
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		return ResponseEntity.ok(postService.findById(id));
-	}
-
-	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Post post) {
-		post = postService.insert(post);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-
-	@DeleteMapping("{id}")
-	public ResponseEntity<Void> delete(@PathVariable String id) {
-		postService.delete(id);
-		return ResponseEntity.noContent().build();
-	}
-
-	@PutMapping
-	public ResponseEntity<Void> update(@RequestBody Post post) {
-		postService.update(post);
-		return ResponseEntity.noContent().build();
-
 	}
 
 }
--- src/main/java/br/edu/ifs/workshop/mongo/services/PostService.java
+++ src/main/java/br/edu/ifs/workshop/mongo/services/PostService.java
@@ -1,9 +1,7 @@
 package br.edu.ifs.workshop.mongo.services;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,27 +15,9 @@ public class PostService {
 	@Autowired
 	private PostRepository postRepository;
 
-	public List<Post> findAll() {
-		return postRepository.findAll();
-	}
-	
 	public Post findById(String id) {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public Post insert(Post post) {
-		return postRepository.insert(post);
-	}
-	
-	public void delete(String id) {
-		postRepository.delete(findById(id));
-	}
-	
-	public Post update(Post post) {
-		Post newPost = findById(post.getId());
-		BeanUtils.copyProperties(post, newPost);
-		return postRepository.save(newPost);
-	}
-	
+
 }
