be0ce8297e172f63a8606d13c67da9869b53417f
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-09-01 15:35:58 -0300

    Acrescentando comentário aos posts

--- src/main/java/br/edu/ifs/workshop/mongo/config/Instantiation.java
+++ src/main/java/br/edu/ifs/workshop/mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.edu.ifs.workshop.mongo.domain.Post;
 import br.edu.ifs.workshop.mongo.domain.User;
 import br.edu.ifs.workshop.mongo.dto.AuthorDTO;
+import br.edu.ifs.workshop.mongo.dto.CommentDTO;
 import br.edu.ifs.workshop.mongo.repositories.PostRepository;
 import br.edu.ifs.workshop.mongo.repositories.UserRepository;
 
@@ -39,6 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		Post p1 = new Post(null, new Date(System.currentTimeMillis()), "Mas que loucura",
 				"Juro que vi a maria conversando com a julia, logo a maria que tinha brigado feio com a julia", new AuthorDTO(bob));
 		Post p2 = new Post(null, new Date(System.currentTimeMillis() + 1928374659128L), "Bom dia", "Bom dia povo!", new AuthorDTO(marilia));
+		
+		
+		CommentDTO c1 = new CommentDTO("Acho que você não devia fofocar tanto mestre.", new Date(System.currentTimeMillis() + 98127364L), new AuthorDTO(alex));
+		
+		CommentDTO c2 = new CommentDTO("Bom dia pra você também moça.", new Date(System.currentTimeMillis()+ 9998374659128L), new AuthorDTO(alex));
+		
+		p1.getComments().add(c1);
+		p2.getComments().add(c2);
 		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
